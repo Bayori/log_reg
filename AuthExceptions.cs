da0215bcf2c355e3e209258515916b4c03302e89
Nekorise <32713344+Bayori@users.noreply.github.com>
2022-11-23 17:45:55 +0500

    Код с классами исключений

---
+++ AuthExceptions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace AuthExceptions
+{
+    public class UserDoesNotExistsException : Exception // Пользователя не существует
+    {
+        public UserDoesNotExistsException(string message) : base(message) { }
+    }
+    public class IncorrectPasswordException : Exception // Пароль неверный
+    {
+        public IncorrectPasswordException(string message) : base(message) { }
+    }
+    public class PasswordConfirmMismatchException : Exception // Пароли не совпадают
+    {
+        public PasswordConfirmMismatchException(string message) : base(message) { }
+    }
+    public class UserAlreadeExistException : Exception // Пользователь существует
+    {
+        public UserAlreadeExistException(string message) : base(message) { }
+    }
+    public class IncorrectLoginException : Exception // Логин некорректный
+    {
+        public IncorrectLoginException(string message) : base(message) { }
+    }
+}
